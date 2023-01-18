524ef13e3d75591004443898e551fd45049d799e
Diamond Mccall <singlikediamond@gmail.com>
2023-01-18 17:55:43 -0600

    Inital commit

--- MethodsExercise/Program.cs
+++ MethodsExercise/Program.cs
@@ -1,10 +1,29 @@
-﻿namespace MethodsExercise
+﻿namespace
+    MethodsExercise
 {
-    public class Program
+    class Program
     {
+        public static void HeyYou()
+
+        {
+            Console.WriteLine($"Hey you! What's your name?");
+            var userName = Console.ReadLine();
+
+            Console.WriteLine($"Nice to meet you,{userName}! My favorite color is yellow, what's yours?");
+            var color = Console.ReadLine();
+
+            Console.WriteLine($"I love {color}! What's your favorite animal {userName}?");
+            var animal = Console.ReadLine();
+
+            Console.WriteLine($" {animal}?! I would have never guessed. What about music? Who is your favorite band?");
+            var band = Console.ReadLine();
+
+            Console.WriteLine($"I've heard of {band}!");
+        }
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            HeyYou();
         }
+
     }
 }
