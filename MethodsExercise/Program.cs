3e40efc5e263f95841f3a937404658840a9d91b0
Diamond Mccall <singlikediamond@gmail.com>
2023-01-18 18:15:28 -0600

    2ndcommit

--- MethodsExercise/Program.cs
+++ MethodsExercise/Program.cs
@@ -15,10 +15,27 @@
             Console.WriteLine($"I love {color}! What's your favorite animal {userName}?");
             var animal = Console.ReadLine();
 
-            Console.WriteLine($" {animal}?! I would have never guessed. What about music? Who is your favorite band?");
+            Console.WriteLine($"{animal}?! I would have never guessed. What about music? Who is your favorite band?");
             var band = Console.ReadLine();
 
             Console.WriteLine($"I've heard of {band}!");
+
+            Console.WriteLine("-----------------------------------------");
+
+            Console.WriteLine($"Here's a little profile of what I've learned about you today,{userName}!");
+
+            Console.WriteLine($"You like,");
+
+
+            Console.WriteLine ($"{color}");
+
+            Console.WriteLine($"{animal}");
+
+            Console.WriteLine($"{band}");
+
+
+
+
         }
         static void Main(string[] args)
         {
